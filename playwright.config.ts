3c41acffafef6922ba69023e9364daf8166698bf
Rebecca Tang <2001rebecca.tang@gmail.com>
2026-05-08 14:14:32 +0200

    add e2e tests to pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
-  workers: 4,
+  workers: process.env.CI ? 2 : 4,
   reporter: [['html'], ['list']],
   globalSetup: './e2e/support/global-setup.ts',
   use: {
